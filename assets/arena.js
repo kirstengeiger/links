730178966409361b25a023ed15ce220793cbb8a4
kirstengeiger <geigk206@newschool.edu>
2024-02-27 00:30:56 -0500

    Image: Added overflow-scroll

--- assets/arena.js
+++ assets/arena.js
@@ -121,7 +121,7 @@ let renderBlock = (block) => {
 							</section>
 							<section class="image-thumbnail">
 								<source media="(max-width: 640px)" srcset="${block.image.thumb.url}">
-								<img src="${block.image.thumb.url}" alt="${block.title}" by "${block.user.fullname}" width="600" height="700">
+								<img src="${block.image.thumb.url}" alt="${block.title}" by "${block.user.fullname}" width="100%" height="900px">
 							</section>
 						</section>
 						<section class="images-description-onclick">
